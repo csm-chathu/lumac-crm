248a4d376d695e3ea016b1f0a00cd0919a077485
chathuranga <chaturanga@itone.biz>
2026-05-20 23:14:31 +0530

    gg

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (! $this->app->isLocal()) {
+            // Prevent accidental usage of the local Vite dev server on shared hosting.
+            Vite::useHotFile(storage_path('framework/vite.hot'));
+        }
     }
 }
